7c70a523e8500569277ca2394bb6e2aef7721f21
Austin Carey <austinpatrickcarey@gmail.com>
2017-07-19 14:58:10 -0400

    time for css

--- main.js
+++ main.js
@@ -1,14 +1,32 @@
-let searchbar = document.querySelector(".search");
-let item = document.querySelector(".itemlist")
-fetch("https://crossorigin.me/http://www.recipepuppy.com/api/?q=" + (recipe.search.value))
-  .then(function(obj){
-    return obj.json()
-    .then(function(obj){
-    obj.results.forEach(function(loop){
+let input = document.querySelector(".input")
+let output = document.querySelector(".output")
 
 
-      console.log(loop)
+input.addEventListener("keypress", function(e) {
+  let key = e.which;
+  if (key === 13) {
+    fetch("https://crossorigin.me/http://www.recipepuppy.com/api/?q=" + input.value)
 
-    })
-    })
-  });
+      .then(function(response) {
+        if (response.status !== 200) {
+          console.log(response);
+        }
+        response.json().then(function(obj) {
+          console.log(obj);
+
+          let template;
+          obj.results.forEach(function(data) {
+            template = `
+            <div class="recipes">
+              <a href="${data.href}">
+              <img src="${data.thumbnail}" onError="this.onerror=null;this.src='puppy.png'">
+              <h3>${data.title}</h3>
+              </a>
+            </div>
+            `;
+            output.innerHTML += template;
+          })
+        })
+      });
+  }
+});
